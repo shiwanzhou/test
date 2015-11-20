8ec6662273137d847eae2b2e6930986446d4a6af
shiwanzhou <737816277@qq.com>
2015-11-20 21:11:21 +0800

    f
    f

--- js/m-reactive.js
+++ js/m-reactive.js
@@ -32,7 +32,7 @@
             if(p_w < 300){
                 fz = p_w/16;
             }else if(p_w >= 300 && p_w <= 640){
-                fz = p_w/12;
+                fz = p_w/14;
             }else{
                 fz = 40;
             }
